f1c4e94aafc498e02649f988eb459164167cb412
CxrlosKenobi <yolitopinto@gmail.com>
2021-08-08 00:09:40 +0000

    Update js

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const client = new Discord.Client()
 const config = require('./config.json')
 const command = require('./command')
 const roleClaim = require('./role-claim')
+const welcome = require('./welcome')
 
 client.on('ready', () => {
     console.log('The client is ready!')
+    welcome(client)
     roleClaim(client)
 
     command(client, ['ping', 'TestingBot'], (message) => {
